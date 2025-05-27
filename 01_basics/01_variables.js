3da5ef42729d4a35b63be4327ce2408b5120dae8
mushfiqarnab <155722846+mushfiqarnab@users.noreply.github.com>
2025-05-27 17:44:00 +0000

    VariableTesting

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12345 //not changeable 
+let accountEmail = "abc@dmail.com"
+var accountPassword = '12345'
+accountCity = "Dhaka"
+
+// accountId = 53222
+
+console.log(accountId);
+
+accountEmail = "@bcd@email.com"
+accountPassword = "54321"
+accountCity = "Barisal"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
