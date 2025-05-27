b9f51bfcdbf9e00ca16f4ca6967084c111ff43f2
mushfiqarnab <155722846+mushfiqarnab@users.noreply.github.com>
2025-05-27 18:13:43 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use stirct"; //treat all js code as newer version
+// alert(3+3) // we are using nodejs  not browser
+console.log(3+3) 
+//code readability should be high
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+//number ==> 2 to the power 52
+//bigint ==> when number is too big
+//string ==> ""
+//boolean ==> true/false
+//null ==> standalone valu
+//undefined ==> let state; value not assigned yet
+//symbol ==> to find uniqueness 
+
+
+//object
+
+console.log(typeof "hitesh");
+console.log(typeof age)
+console.log(typeof null) // object type
+console.log(typeof undefined)
+
